e6f0dc1380c58b76856ee42e5648729d59ab0ba9
Gizachew <72370435+Gizachew29@users.noreply.github.com>
2021-03-28 17:55:47 -0700

    Update import.py

--- pset7/houses/import.py
+++ pset7/houses/import.py
@@ -2,6 +2,7 @@ import csv
 from sys import argv, exit
 from cs50 import SQL
 
+
 if len(argv) != 2:
     exit()
 
@@ -11,16 +12,21 @@ with open(argv[1], "r") as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
         name_arr = row['name'].split(" ")
+        
+        
 
         if len(name_arr) < 3:
             first_name = name_arr[0]
             last_name = name_arr[1]
             middle_name = None
+            
+            
         else:
             first_name = name_arr[0]
             last_name = name_arr[2]
             middle_name = name_arr[1]
         db.execute("INSERT INTO students (first, middle, last, house, birth) VALUES (?, ?, ?, ?, ?)", first_name, middle_name, last_name, row['house'], row['birth'])
+        
 
 # CREATE TABLE students (
 #     id INTEGER PRIMARY KEY AUTOINCREMENT,
